cc871b77a44685d5eb211811fb1c5a5a74b3919a
sincerefly <kissbug8720@gmail.com>
2023-01-10 20:34:40 +0800

    fix: viper can get right IsSet result now

--- cmd/funcs.go
+++ cmd/funcs.go
@@ -6,12 +6,6 @@ import (
 )
 
 // getParamB returns a parameter as a string and a boolean to tell if it is different from the default
-//
-// NOTE: we could simply bind the flags to viper and use IsSet.
-// Although there is a bug on Viper that always returns true on IsSet
-// if a flag is binded. Our alternative way is to manually check
-// the flag and then the value from env/config/gotten by viper.
-// https://github.com/spf13/viper/pull/331
 func getParamB(flags *pflag.FlagSet, key string) (string, bool) {
 	value, _ := flags.GetString(key)
 
