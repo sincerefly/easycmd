db40dfaf98a8698185d2f492407c430e6d671a34
sincerefly <kissbug8720@gmail.com>
2023-01-18 15:47:12 +0800

    chore: test && ci script filename

--- utils/random/random_test.go
+++ utils/random/random_test.go
@@ -6,14 +6,21 @@ import (
 )
 
 func TestChoice(t *testing.T) {
-	v := Choice([]int{1})
-	assert.Equal(t, 1, v, "random choice")
+	if v := Choice([]int{1}); v != 1 {
+		t.Errorf("choice {1} expected 1, but %d got", v)
+	}
+	if v := Choice([]int{1, 2}); v != 1 && v != 2 {
+		t.Errorf("choice {1,2} expected 1 or 2, but %d got", v)
+	}
+	if v := Choice([]float64{1.1, 2.2}); v != 1.1 && v != 2.2 {
+		t.Errorf("choice {1,2} expected 1 or 2, but %f got", v)
+	}
 }
 
 func TestContains(t *testing.T) {
 	has := Contains([]int{1, 2, 3}, 1)
-	assert.Equal(t, true, has, "contains 1")
+	assert.Equal(t, true, has, "contains {1,2,3} value 1 expected true")
 
 	has = Contains([]int{1, 2, 3}, 4)
-	assert.Equal(t, false, has, "not contains 4")
+	assert.Equal(t, false, has, "contains {1,2,3} value 4 expected false")
 }
