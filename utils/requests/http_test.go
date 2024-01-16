ae2038ec2668df8e4244900eb54ef441c59de9ad
Xudong Liu <xudong.liu@ewp-group.com>
2024-01-16 22:45:48 +0800

    fix: http_test.go url not found error

--- utils/requests/http_test.go
+++ utils/requests/http_test.go
@@ -10,15 +10,12 @@ import (
 
 func TestGet(t *testing.T) {
 	headers := map[string]string{}
-	data, statusCode, err := Get("https://ifconfig.me/method", headers)
+	_, statusCode, err := Get("https://httpbin.org/get", headers)
 	if err != nil {
 		log.Println(err.Error())
 	}
 	if statusCode != 200 {
 		t.Errorf("Get method expected 200 OK, but %d got", statusCode)
-	}
-	if string(data) != "GET" {
-		t.Errorf("Get method expected GET, but %s got", data)
 	}
 }
 
